05edc1543c8991d45bf68a2bb62df9c5e1353db5
SrZitu <zitu094@gmail.com>
2021-01-16 11:20:22 +0600

    2nd commit

--- admin/index.php
+++ admin/index.php
@@ -2,28 +2,28 @@
 
 <div id="wrapper">
 
-    <!-- Navigation -->
-    <?php include "includes/nav.php" ?>
+  <!-- Navigation -->
+  <?php include "includes/nav.php" ?>
 
-    <div id="page-wrapper">
+  <div id="page-wrapper">
 
-        <div class="container-fluid">
+    <div class="container-fluid">
 
-            <!-- Page Heading -->
-            <div class="row">
-                <div class="col-lg-12">
-                    <h1 class="page-header">
-                        Welcome to Admin
-                        <small>author</small>
-                    </h1>
-
-                </div>
-            </div>
-            <!-- /.row -->
+      <!-- Page Heading -->
+      <div class="row">
+        <div class="col-lg-12">
+          <h1 class="page-header">
+            Welcome to Admin
+            <small><?php echo $_SESSION['username']; ?></small>
+          </h1>
 
         </div>
-        <!-- /.container-fluid -->
+      </div>
+      <!-- /.row -->
 
     </div>
-    <!-- /#page-wrapper -->
-    <?php include "includes/footer.php" ?>
+    <!-- /.container-fluid -->
+
+  </div>
+  <!-- /#page-wrapper -->
+  <?php include "includes/footer.php" ?>
